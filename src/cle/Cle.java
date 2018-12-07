a18aa732f289532faa2c2b0a052ffe9e5885bd57
Skelly98 <44553011+Skelly98@users.noreply.github.com>
2018-12-07 19:22:19 +0100

    Update Cle.java

--- src/cle/Cle.java
+++ src/cle/Cle.java
@@ -9,17 +9,16 @@ public class Cle {
 	
 	//bit de poids faible : indice 3
 	public Cle(int i1,int i2,int i3,int i4) {
-		this.cle[0] = i1;
-		this.cle[1] = i2;
-		this.cle[2] = i3;
-		this.cle[3] = i4;
+		cle[0] = i1;
+		cle[1] = i2;
+		cle[2] = i3;
+		cle[3] = i4;
 	}
 	
 	//question 1.1
 	public boolean inf(Cle c) {
 		for(int i =0; i<4;i++) {
-			if (this.cle[i] < c.getCle()[i]) return true;
-			if (this.cle[i] > c.getCle()[i]) break;
+			if (cle[i] < c.getCle()[i]) return true;
 		}
 		return false;
 	}
@@ -28,13 +27,9 @@ public class Cle {
 	//question 1.2
 	public boolean eg(Cle c) {
 		for (int i= 0;i<4;i++) {
-			if(this.cle[i] !=  c.getCle()[i]) return false;
+			if(cle[i] !=  c.getCle()[i]) return false;
 		}
 		return true;
-	}
-	
-	public int [] getCle() {
-		return this.cle;
 	}
 	
 	public void setCle(Cle c) {
