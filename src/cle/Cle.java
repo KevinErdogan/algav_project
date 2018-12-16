9d03fc1699e32212c301ec24c7012d6385fa8b3e
Kev <kev.erdogan@gmail.com>
2018-12-16 01:22:37 +0100

    awesome update

--- src/cle/Cle.java
+++ src/cle/Cle.java
@@ -4,11 +4,15 @@ package cle;
 
 public class Cle {
 
-	private int [] cle = new int [4];
+	private long [] cle;
 	
+	public Cle() {
+		cle = new long[] {0,0,0,0};
+	}
 	
 	//bit de poids faible : indice 3
-	public Cle(int i1,int i2,int i3,int i4) {
+	public Cle(long i1,long i2,long i3,long i4) {
+		cle = new long[4];
 		cle[0] = i1;
 		cle[1] = i2;
 		cle[2] = i3;
@@ -17,9 +21,9 @@ public class Cle {
 	
 	//question 1.1
 	public boolean inf(Cle c) {
-		for(int i =0; i<4;i++) {
-			if (cle[i] < c.getCle()[i]) return true;
-		}
+		for(int i =0; i<4;i++) 
+			if (cle[i] != c.cle[i]) 
+				return cle[i] < c.cle[i];
 		return false;
 	}
 	
@@ -27,16 +31,17 @@ public class Cle {
 	//question 1.2
 	public boolean eg(Cle c) {
 		for (int i= 0;i<4;i++) {
-			if(cle[i] !=  c.getCle()[i]) return false;
+			if(cle[i] !=  c.cle[i])
+				return false;
 		}
 		return true;
 	}
 	
-	public void setCle(Cle c) {
-		this.cle = c.cle;
+	public long [] getCle() {
+		return cle;
 	}
 	
 	public String toString() {
-		return ""+this.cle[0]+ ""+this.cle[1]+""+this.cle[2]+""+this.cle[3];
+		return ""+cle[0]+" "+cle[1]+" "+cle[2]+" "+cle[3];
 	}
 }
--- src/fileBinomiale/FileBinomiale.java
+++ src/fileBinomiale/FileBinomiale.java
@@ -1,5 +1,151 @@
 package fileBinomiale;
 
-public class FileBinomiale {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
+import cle.Cle;
+
+public class FileBinomiale {
+	private List<TournoiBinomiale> tournois = new LinkedList<TournoiBinomiale>();
+	
+	public FileBinomiale() {}
+	
+	public FileBinomiale(List<TournoiBinomiale> binomials) {
+		for(TournoiBinomiale t : binomials)
+			tournois.add(t);
+	}
+	
+	// primitives
+
+	public boolean estVide() {
+		return tournois.isEmpty();
+	}
+	
+	public TournoiBinomiale minDeg() {
+		if(tournois.isEmpty()) return null;
+		TournoiBinomiale minDeg = tournois.get(0);
+		for(int i=1; i<tournois.size();i++) {
+			if(tournois.get(i).degre() < minDeg.degre())
+				minDeg=tournois.get(i);
+		}
+		return minDeg;
+	}
+	
+	public FileBinomiale reste() {
+		TournoiBinomiale degMin = minDeg();
+		tournois.remove(degMin);
+		FileBinomiale reste = new FileBinomiale(tournois);
+		tournois.add(degMin);
+		return reste;
+	}
+	
+	public void ajoutMin(TournoiBinomiale t) {
+		tournois.add(t);
+	}
+	
+	// fonctions fondamentales
+	
+	public void supprMin() {
+		if(tournois.isEmpty()) return;
+		TournoiBinomiale toRemove = tournois.get(0);
+		for(int i =1;i<tournois.size();i++) 
+			if(tournois.get(i).getRacine().getVal().inf(toRemove.getRacine().getVal())) 
+				toRemove=tournois.get(i);
+		FileBinomiale f = toRemove.decapite();
+		tournois.remove(toRemove);
+		union(f);
+	}
+	
+	public void ajout(TournoiBinomiale t) {
+		union(TournoiBinomiale.file(t));
+	}
+	
+	public FileBinomiale union(FileBinomiale f) {
+		FileBinomiale copy = new FileBinomiale(f.tournois);
+		List<TournoiBinomiale> newT = new LinkedList<TournoiBinomiale>();
+		TournoiBinomiale t1, t2, memory=null;
+		
+		while(!tournois.isEmpty() && !copy.tournois.isEmpty()){
+			t1 = minDeg();
+			t2 = copy.minDeg();
+			if(memory!=null) {
+				if(memory.degre()==t1.degre() && memory.degre()==t2.degre()) {
+					newT.add(memory);
+					memory=t1.union2Tid(t2);
+					tournois.remove(t1);
+					copy.tournois.remove(t2);
+				}else if(memory.degre()==t1.degre()) {
+					memory = memory.union2Tid(t1);
+					tournois.remove(t1);
+				}else if(memory.degre()==t2.degre()){
+					memory = memory.union2Tid(t2);
+					copy.tournois.remove(t2);
+				}else {
+					newT.add(memory);
+					memory=null;
+				}
+			}else if(	t1.degre() < t2.degre()) {
+					newT.add(t1);
+					tournois.remove(t1);
+			}
+			else if(t1.degre() > t2.degre()){
+				newT.add(t2);
+				copy.tournois.remove(t2);
+			} else {
+				memory = t1.union2Tid(t2);
+				tournois.remove(t1);
+				copy.tournois.remove(t2);
+			}
+		}
+		tournois = newT;
+		return this;
+	}
+	
+	public void ConsIter(List<Cle> cles) {
+		for(Cle c : cles)
+			ajout(new TournoiBinomiale(c));
+	}
+	
+	// Test Area
+	
+	public static void main(String[] args) {
+		LinkedList<Cle> cles = new LinkedList<Cle>();
+		try {
+			FileReader f = new FileReader(new File("cles_alea/jeu_1_nb_cles_50000.txt"));
+			char buffer0x[] = new char[2];
+			String s = "";
+			int turn=3;
+			long values[] = new long[] { 0, 0, 0, 0 };
+			char a = 0;
+			while(f.read(buffer0x)>0) {
+				while((a= ((char) f.read()))!='\n' && s.length()<=32)
+					s = s + a;
+				while(s.length()>=8) {
+					values[turn]=Long.parseLong(s.substring(s.length()-8,s.length()), 16);
+					turn--;
+					s=s.substring(0, s.length()-8);
+				}
+				if(turn>-1)
+					values[turn]=Long.parseLong(s, 16);
+				cles.add(new Cle(values[0], values[1], values[2], values[3]));
+				values[0]=0;values[1]=0;values[2]=0;values[3]=0;
+				s="";
+				turn=3;
+			}
+			f.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Nombre de cles : "+cles.size());
+		System.out.println("Test de la complexite temporelle de la fonction ConsIter");
+		FileBinomiale f = new FileBinomiale();
+		System.out.println("Start !");
+		long startTime = System.currentTimeMillis();
+		f.ConsIter(cles);
+		long endTime = System.currentTimeMillis();
+		System.out.println("End ! Time : "+(endTime-startTime));
+	}
 }
