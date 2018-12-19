bd43f57c6e01ec5f38ed20f48c875b7ecb8bd16b
Skelly98 <44553011+Skelly98@users.noreply.github.com>
2018-12-19 11:16:14 +0100

    mauvais hachage

--- src/md5/Md5.java
+++ src/md5/Md5.java
@@ -2,7 +2,10 @@ package md5;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Arrays;
+import java.nio.IntBuffer;
+
+
+import cle.Cle;
 
 public class Md5 {
 	
@@ -23,50 +26,25 @@ public class Md5 {
 	public Md5() {
 		k = new int [64];
 		for (int i = 0; i < k.length; i++) {
-			k[i] =  (int) Math.floor(Math.abs(Math.sin(i + 1)) * Math.pow(2, 32));
+			k[i] =  (int) (long) Math.floor(Math.abs(Math.sin(i + 1)) * Math.pow(2, 32));
 		}
 	}
 	
 	
 	public String md5(String s) {
 		
-		//taile padding
-		int  sizePadding = s.length()+1;
-		
-		while(( sizePadding % 512) != 448) {
-			 sizePadding++;
-		}
-		sizePadding +=64;
-		
-		byte[] padding = Arrays.copyOf(s.getBytes(),  sizePadding/8);
-		//System.out.println(padding.length);
-		padding[s.length()] = (byte) 128;
-		 
-		//taille message en 64 bits
-		 long messageSize = s.length() * 8;
-		 ByteBuffer bb = ByteBuffer.allocate(Long.BYTES).order(ByteOrder.LITTLE_ENDIAN);
-		 bb.putLong(messageSize);
-		 
-		 byte [] bit64_m  = bb.array();
-		 int cmp =0;
-		 for(int i = padding.length-8;i< padding.length;i++) {
-			 padding[i] = bit64_m[cmp];
-			 cmp++;
-		 }
-		
-		 ByteBuffer ib;
-		 for (int j = 0; j<padding.length;j+=64) {
+		byte [] message = s.getBytes();
+		ByteBuffer bb = ByteBuffer.allocate((((message.length + 8) / 64) + 1) * 64).order(ByteOrder.LITTLE_ENDIAN);
+	    bb.put(message);
+	    bb.put((byte)0x80);
+	    long messageLenBits = (long)message.length * 8;
+	    bb.putLong(bb.capacity() - 8, messageLenBits);
+	 
+	    bb.rewind();
+		while(bb.hasRemaining()) {
 			 
-			 int tmp = j;
-			 int[] w = new int [16];
-			 for(int i = 0; i<w.length;i++) {
-				 byte [] b = {padding[tmp],padding[tmp+1],padding[tmp+2],padding[tmp+3]};
-				 ib = ByteBuffer.wrap(b).order(ByteOrder.LITTLE_ENDIAN);
-				 w[i] = ib.getInt();
-				 tmp +=4;
-			 }
-
-		
+		    IntBuffer w = bb.slice().order(ByteOrder.LITTLE_ENDIAN).asIntBuffer();
+		    
 			int a = h0;
 			int b = h1;
 			int c = h2;
@@ -79,23 +57,24 @@ public class Md5 {
 					f = (b & c) | (~b & d);
 					g = i;
 				}
-				if(i<32) {
-					f = (d & b) | (~d & c);
-					d = (5*i +1) % 16;
+				else if(i<32) {
+					f = (b & d) | (~d & c);
+					g = (5*i + 1) % 16;
 				}
-				if(i<47) {
-					f = c ^ b ^ d;
-					g = (3*i +5) % 16;
+				else if(i<47) {
+					f = b ^ c ^ d;
+					g = (3*i + 5) % 16;
 				}
 				else {
 					f = c ^ (b | ~d);
 					g = (7*i) % 16;
 				}
 				
-				temp = d;
+			    temp = d;
 				d = c;
 				c = b;
-				b = Integer.rotateLeft(a + f + k[i]+ w[g], r[i])+b;
+				b = Integer.rotateLeft(a + f + k[i]+ w.get(g), r[i])+b;
+				System.out.println(w.get(g));
 				a = temp;
 					
 			}
@@ -104,14 +83,18 @@ public class Md5 {
 			h1 = h1 + b;
 			h2 = h2 + c;
 			h3 = h3 + d;
+			bb.position(bb.position() + 64);
 		 }
-			
+		
+		 
 		 	ByteBuffer res = ByteBuffer.allocate(16).order(ByteOrder.LITTLE_ENDIAN);
 		 	res.putInt(h0);
 		 	res.putInt(h1);
 		 	res.putInt(h2);
 		 	res.putInt(h3);
 		    return toHexString(res.array());
+		 
+		// return new Cle (h0,h1,h2,h3);
 		  }
 
 	
@@ -125,7 +108,13 @@ public class Md5 {
 	    return sb.toString();
 	  }
 
+	
+	public static void main(String[] args) {
+		Md5 md = new Md5();
+		
+		System.out.println("0X"+md.md5("a"));
+	
+	}
 	}
 
 	
-
